6c3f2594e3f24e71c9a511d2e482eb9813607261
Jerlissa Rivas <jerlissa.rivas@gmail.com>
2020-06-15 23:38:58 -0400

    Complete Clicker using React hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+function ClickCountButton() {
+  const state = useState(0);
+  const count = state[0];
+  const setCount = state[1];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button onClick={() => setCount(count + 1)}>
+      You have clicked {count} times!
+    </button>
   );
 }
 
-export default App;
+export default ClickCountButton;
